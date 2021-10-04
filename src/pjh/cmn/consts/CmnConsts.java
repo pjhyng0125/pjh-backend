e3458c8d8126487ac4f3b3999f16e635d93c3054
pjhyng0125 <pjhyng0125@naver.com>
2021-10-04 21:44:46 +0900

    211004/pjh/공통상수 변경 (파일 구분 추가)

--- src/pjh/cmn/consts/CmnConsts.java
+++ src/pjh/cmn/consts/CmnConsts.java
@@ -1,8 +1,23 @@
 package pjh.cmn.consts;
 
-public class CmnConsts {
+public class CmnConsts {	
 	/*
-	 * 프로젝트 루트 경로
+	 * 파일 생성 공유 디렉토리 루트 경로
 	 * */
-	public static String PJT_ROOT_PATH = "C:\\pjh\\works\\eclipse\\pjh-backend\\src\\data\\";
+	public static String SHARE_ROOT_PATH = "C:\\pjh\\works\\eclipse\\pjh-backend\\share\\";
+	
+	/*
+	 * TEXT 파일 구분 문자열
+	 * */
+	public static String TXT_FILE_DIV = "txt";
+	
+	/*
+	 * JSON 파일 구분 문자열
+	 * */
+	public static String JSON_FILE_DIV = "json";
+	
+	/*
+	 * PDF 파일 구분 문자열
+	 * */
+	public static String PDF_FILE_DIV = "pdf";
 }
