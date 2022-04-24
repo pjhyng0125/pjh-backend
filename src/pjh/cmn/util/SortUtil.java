508c2160d0576063283d1d1f2ca1bac9343d4b07
pjhyng0125 <pjhyng0125@naver.com>
2022-04-25 05:40:59 +0900

    220425/pjh/미반영 변경사항 커밋

--- src/pjh/cmn/util/SortUtil.java
+++ src/pjh/cmn/util/SortUtil.java
@@ -20,7 +20,6 @@ public class SortUtil {
 	public static int[] bubbleSort(int src[], boolean ascYn) {
 		int len = src.length;
 		int[] dst = src.clone();
-//		int[] dst = Arrays.copyOf(src, len);
 		int tmp = 0;
 		
 		for (int i = 0; i < len - 1; i++) {
@@ -31,6 +30,32 @@ public class SortUtil {
 					dst[j + 1] = tmp;
 				}
 			}
+		}
+		return dst;
+	}
+	
+	/**
+	 * 삽입 정렬
+	 * @param int[] 정수 배열
+	 * @param boolean 오름차순 여부
+	 * @return int[]
+	 * 
+	 */
+	public static int[] selectSort(int src[], boolean ascYn) {
+		int len = src.length;
+		int[] dst = Arrays.copyOf(src, len);
+		int tmp, idx = 0;
+		
+		for (int i = 0; i < len - 1; i++) {
+			idx = i;
+			for (int j = i + 1; j < len; j++) {
+				if (ascYn ? dst[j] < dst[idx] : dst[j] > dst[idx]) {
+					idx = j;
+				}
+			}
+			tmp = dst[i];
+			dst[i] = dst[idx];
+			dst[idx] = tmp;
 		}
 		return dst;
 	}
